36581a2889f2a41933b51097ec8bf323dabc746a
Chen Xiaohui <graych@gmail.com>
2017-05-14 20:48:01 +0800

    简化 consul-ha

--- scripts/consul-ha-support.sh
+++ scripts/consul-ha-support.sh
@@ -6,10 +6,7 @@ sync_begin(){
 	[ -z "$SYNC_LOCK_HELD" ] && return 1
 
 	rm -f $INFRA_DIR/*.*
-	local STATE_SYNC
-	consul kv get "-http-addr=$CONSUL_ENDPOINT" -keys "$CONSUL_HA/" || return 1
-	consul kv get "-http-addr=$CONSUL_ENDPOINT" "$CONSUL_HA/.sync" >$INFRA_DIR/.sync && STATE_SYNC="$(cat $INFRA_DIR/.sync)" || return 0
-	consul kv get "-http-addr=$CONSUL_ENDPOINT" -keys "$CONSUL_HA/$STATE_SYNC" >$INFRA_DIR/.keys || return 1
+	consul kv get "-http-addr=$CONSUL_ENDPOINT" -keys "$CONSUL_HA/" >$INFRA_DIR/.keys || return 1
 
 	local STATUS SYNC_ITEM
 	[ -f $INFRA_DIR/.keys ] && while read -r SYNC_ITEM; do
@@ -24,27 +21,16 @@ sync_begin(){
 sync_end(){
 	[ -z "$SYNC_LOCK_HELD" ] && return 1
 	
+	consul kv get "-http-addr=$CONSUL_ENDPOINT" -keys "$CONSUL_HA/" >/dev/null || return 1
 	rm -f $INFRA_DIR/*.json
 
 	local STATUS SYNC_ITEM
 	for SYNC_ITEM in $(find $INFRA_DIR -maxdepth 1 -type f -name '*.*'); do
 		[[ "${SYNC_ITEM##*/}" != '.'* ]] && [ -f $SYNC_ITEM ] && {
 			echo "Saving $SYNC_ITEM"
-			consul kv put "-http-addr=$CONSUL_ENDPOINT" "$CONSUL_HA/$SYNC_LOCK_HELD/${SYNC_ITEM##*/}" "@$SYNC_ITEM" || STATUS=1
+			consul kv put "-http-addr=$CONSUL_ENDPOINT" "$CONSUL_HA/${SYNC_ITEM##*/}" "@$SYNC_ITEM" || STATUS=1
 		}
 	done
-
-	[ -z "$STATUS" ] && {
-		local STATE_SYNC
-		consul kv get "-http-addr=$CONSUL_ENDPOINT" "$CONSUL_HA/.sync" >$INFRA_DIR/.sync && STATE_SYNC="$(cat $INFRA_DIR/.sync)"
-		consul kv put "-http-addr=$CONSUL_ENDPOINT" "$CONSUL_HA/.sync" "$SYNC_LOCK_HELD" || STATUS=1
-		[ ! -z "$STATE_SYNC" ] && [ "$STATE_SYNC" != "$SYNC_LOCK_HELD" ] && \
-			consul kv delete "-http-addr=$CONSUL_ENDPOINT" -recurse "$CONSUL_HA/$STATE_SYNC"
-	}
-
-	[ ! -z "$STATUS" ] && {
-		consul kv delete "-http-addr=$CONSUL_ENDPOINT" -recurse "$CONSUL_HA/$SYNC_LOCK_HELD"
-	}
 
 	return ${STATUS:-0}
 }
